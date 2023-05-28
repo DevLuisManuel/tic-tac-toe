bdc41a0f5a135b1f28ee098a45e72c93d33aa491
Luis Manuel Zuñiga Moreno <ing.luiszunigam@gmail.com>
2023-05-28 11:18:23 -0500

    feat(board):
    I already have the new board and the game turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
-import './App.css'
+import {useState} from "react";
+
+const TURNS = {
+    X: 'x',
+    O: 'o'
+}
+const Square = ({children, isSelected, updateBoard, index}) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    return (
+        <div onClick={handleClick} className={className}>
+            {children}
+        </div>
+    )
+}
 
 function App() {
-    return <h1>Tic Tac Toe</h1>
+    const [board, setBoard] = useState(
+        Array(9).fill(null)
+    );
+    const [turn, setTurn] = useState(TURNS.X);
+
+    const updateBoard = (index) => {
+        const newBoard = [...board]
+        newBoard[index] = turn
+        setBoard(newBoard)
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+        setTurn(newTurn)
+
+    }
+
+    console.log(board)
+    return (
+        <main className="board">
+            <h1>Tic Tac Toe</h1>
+            <section className="game">
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square key={index} index={index} updateBoard={updateBoard}>
+                                {board[index]}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+            <section className="turn">
+                <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+                <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+            </section>
+        </main>
+    );
 }
 
 export default App
