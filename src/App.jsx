8fe49b863af9a31dcbab3389f273fc99e4af1f17
Luis Manuel Zuñiga Moreno <ing.luiszunigam@gmail.com>
2023-05-28 12:03:12 -0500

    feat(board):
    Finally Tic Tac Toe

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,69 @@ const Square = ({children, isSelected, updateBoard, index}) => {
     )
 }
 
-function App() {
+const WINNER_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const checkWinner = (boardToCheck) => {
+    // Aqui revisamos todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a]
+        }
+    }
+    return null
+}
+const App = () => {
     const [board, setBoard] = useState(
         Array(9).fill(null)
     );
     const [turn, setTurn] = useState(TURNS.X);
+    const [winner, setWinner] = useState(null);
 
     const updateBoard = (index) => {
+        // Verificamos si ya hay algo dentor del index
+        if (board[index] || winner) return
+        // Actualizamos el tablero
         const newBoard = [...board]
         newBoard[index] = turn
         setBoard(newBoard)
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
         setTurn(newTurn)
-
+        const newWinner = checkWinner(newBoard)
+        if (newWinner) {
+            setWinner(newWinner)
+        } else if (checkEndGame(newBoard)) {
+            setWinner(false)
+        }
     }
 
-    console.log(board)
+    const resetGame = () => {
+        setBoard(Array(9).fill(null))
+        setTurn(TURNS.X)
+        setWinner(null)
+    }
+
+    const checkEndGame = (newBoard) => {
+        return newBoard.every((square) => square !== null)
+    }
+
     return (
         <main className="board">
             <h1>Tic Tac Toe</h1>
+            <button onClick={resetGame}>Reset del Juego</button>
             <section className="game">
                 {
                     board.map((_, index) => {
@@ -50,6 +94,27 @@ function App() {
                 <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
                 <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
             </section>
+            {
+                winner !== null && (
+                    <section className="winner">
+                        <div className="text">
+                            <h2>
+                                {
+                                    winner === false
+                                        ? 'Empate'
+                                        : 'Ganó'
+                                }
+                            </h2>
+                            <header className="win">
+                                {winner && <Square>{winner}</Square>}
+                            </header>
+                            <footer>
+                                <button onClick={resetGame}>Empezar de nuevo</button>
+                            </footer>
+                        </div>
+                    </section>
+                )
+            }
         </main>
     );
 }
